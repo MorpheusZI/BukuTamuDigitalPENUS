d9dd0ea48ec56032bd21540a186f38f21771a6de
Morpheus Irawan <morpheus.zein@irawan.id>
2023-03-02 21:03:43 +0700

    bikin anak

--- public/index.js
+++ public/index.js
@@ -7,3 +7,12 @@ const sigpad = new SignaturePad(canvas, {
 clear.addEventListener("click", () => {
   sigpad.clear();
 });
+
+function resizeCanvas() {
+  const ratio = Math.max(window.devicePixelRatio || 1, 1);
+  canvas.width = canvas.offsetWidth + ratio;
+  canvas.height = canvas.offsetHeight * 1 + ratio;
+}
+
+window.onresize = resizeCanvas;
+resizeCanvas();
